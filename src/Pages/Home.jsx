93b8826a2ff067b1398257a0fe62e20a5186b84e
arthasa28 <arthasyarif2@gmail.com>
2023-10-12 12:58:34 +0700

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
 						WELCOME
 					</h1>
 					<h6 className="text-sm" style={{ letterSpacing: "5px" }}>
-						TO XI TKJ 3
+						TO XI TKJ B
 					</h6>
 				</div>
 			</div>
